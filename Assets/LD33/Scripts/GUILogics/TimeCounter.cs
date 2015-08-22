0c190b666d388032c426cc1b702a900602f6225d
sxwx <DeadGeorge>
2015-08-22 13:27:05 +0700

    Added first version of timer

---
+++ Assets/LD33/Scripts/GUILogics/TimeCounter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class TimeCounter : MonoBehaviour {
+
+    public float TimeLeft;
+    private Text _timerControl;
+	// Use this for initialization
+	void Start () {
+        _timerControl = GetComponent<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        TimeLeft -= Time.deltaTime;
+
+        var minutes = TimeLeft / 60;
+        var seconds = TimeLeft % 60;
+
+        _timerControl.text = String.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
