48294d99056eb5074c0821f4b18c3f66ff20d478
almyu <hire@indeed.li>
2015-08-22 22:23:47 +0600

    Add drag noise

--- Assets/LD33/Scripts/Interaction/NoisyObject.cs
+++ Assets/LD33/Scripts/Interaction/NoisyObject.cs
@@ -8,6 +8,8 @@ public class NoisyObject : MonoBehaviour {
     public float alert = 1f;
     public float minCollisionSpeed = 1f;
     public LayerMask collisionLayers = 1; // default
+    public float dragScore = 0f;
+    public float dragAlert = 0f;
 
     private void OnCollisionEnter(Collision collision) {
         if (collision.relativeVelocity.sqrMagnitude < minCollisionSpeed * minCollisionSpeed)
@@ -19,6 +21,19 @@ public class NoisyObject : MonoBehaviour {
             Sfx.Play(sfx);
             AlertCounter.instance.Add(alert);
             ScoreCounter.instance.Add(score);
+        }
+    }
+
+    private void OnCollisionStay(Collision collision) {
+        if (collision.relativeVelocity.sqrMagnitude < minCollisionSpeed * minCollisionSpeed)
+            return;
+
+        foreach (var contact in collision.contacts) {
+            if ((contact.otherCollider.gameObject.layer & collisionLayers.value) == 0) continue;
+
+            //Sfx.Play(sfx);
+            AlertCounter.instance.Add(dragAlert * Time.fixedDeltaTime);
+            ScoreCounter.instance.Add(Mathf.FloorToInt(dragScore * Time.fixedDeltaTime));
         }
     }
 }
