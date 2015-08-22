e6994dc4fa74e8c234dd69d03816395a3fa547cf
almyu <hire@indeed.li>
2015-08-22 13:20:37 +0600

    Add actuator

---
+++ Assets/LD33/Scripts/Interaction/Actuator.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+public class Actuator : MonoBehaviour {
+
+    public string inputButton = "Interact";
+    public UnityEvent action;
+
+    private bool inRange;
+
+    private void OnValidate() {
+        GetComponent<Collider>().isTrigger = true;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        // TODO: input hint
+        inRange = true;
+    }
+
+    private void OnTriggerExit(Collider other) {
+        inRange = false;
+    }
+
+    private void Update() {
+        if (!inRange) return;
+
+        if (Input.GetButtonDown(inputButton))
+            action.Invoke();
+    }
+}
