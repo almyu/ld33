d8d44f3e79d22b8676f6ed22391d400e4b473453
almyu <hire@indeed.li>
2015-08-22 12:54:58 +0600

    Add noisy object script & audio mixer

---
+++ Assets/LD33/Scripts/Interaction/NoisyObject.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using JamSuite.Audio;
+
+public class NoisyObject : MonoBehaviour {
+
+    public string sfxOnBump; //, sfxOnFall;
+    public float minBumpVelocity = 1f; //, minFallVelocity = 10f;
+
+    private void OnCollisionEnter(Collision collision) {
+        // Collide against floor/player layers
+        if (collision.relativeVelocity.sqrMagnitude >= minBumpVelocity * minBumpVelocity)
+            Sfx.Play(sfxOnBump);
+    }
+}
