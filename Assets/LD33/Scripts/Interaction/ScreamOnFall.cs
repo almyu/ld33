98e867a40a58d880a823f7471b7b1e541af6aade
sxwx <DeadGeorge>
2015-08-23 19:16:13 +0700

    Fix scene + add Scream on fall

---
+++ Assets/LD33/Scripts/Interaction/ScreamOnFall.cs
@@ -0,0 +1,21 @@
+﻿using JamSuite.Audio;
+using UnityEngine;
+
+public class ScreamOnFall : MonoBehaviour {
+
+    public string sfx;
+    public float alert = 1f;
+    public float minCollisionSpeed = 1f;
+
+    private void OnCollisionEnter(Collision collision) {
+        if (Mathf.Abs(collision.relativeVelocity.y) < minCollisionSpeed) {
+            return;
+        }
+        
+        foreach (var contact in collision.contacts) {
+            Debug.Log("Ouch!");
+            Sfx.Play(sfx);
+            AlertCounter.instance.Add(alert);
+        }
+    }
+}
