8ce7b1ce12daae89e23db2a311a513ee5792340e
almyu <hire@indeed.li>
2015-08-24 01:31:34 +0600

    Add robot script

---
+++ Assets/LD33/Scripts/Interaction/Robot.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using JamSuite.Audio;
+
+public class Robot : MonoBehaviour {
+
+    public Animation headAnimation;
+    public string alarmSfx = "RobotSpotsPlayer";
+
+    public void OnPlayerSpotted() {
+        SetAnimationSpeed(headAnimation, 0f);
+        Sfx.Play(alarmSfx);
+    }
+
+    public void OnPlayerLost() {
+        SetAnimationSpeed(headAnimation, 1f);
+    }
+
+    public void SetAnimationSpeed(Animation anim, float speed) {
+        foreach (AnimationState state in anim)
+            state.speed = speed;
+    }
+}
