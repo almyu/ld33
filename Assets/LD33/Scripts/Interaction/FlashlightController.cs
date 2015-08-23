dcfa9263cb24b2328dc7de8c4e071d71767bec87
sxwx <DeadGeorge>
2015-08-23 20:28:01 +0700

    Fix of flash light follow (wait before light on)

--- Assets/LD33/Scripts/Interaction/FlashlightController.cs
+++ Assets/LD33/Scripts/Interaction/FlashlightController.cs
@@ -1,13 +1,16 @@
 ﻿using UnityEngine;
 
 public class FlashlightController : MonoSingleton<FlashlightController> {
+
     private Light _flashlight;
     private float elapsed = 0.0f;
     private float strength = 0.5f;
-    
+    private float _waitBeforeTurnOnFlashlight = 4f;
+    private bool _firstTime = true;
     private const float SmoothTime = 0.2F;
     private GameObject _player;
     private LightSpotter _lightSpotter;
+    private Vector3 _oldPlayerPosition;
 
     private void Awake() {
         _flashlight = GetComponent<Light>();
@@ -16,21 +19,45 @@ public class FlashlightController : MonoSingleton<FlashlightController> {
     }
 
     private void Update() {
-        if(elapsed >= Balance.instance.FlashightDuration) {
+        elapsed += Time.deltaTime;
+
+        WaitAndTurnOnFlashLight();
+
+        if (elapsed >= Balance.instance.FlashightDuration + _waitBeforeTurnOnFlashlight) {
             enabled = false;
             return;
         }
-        
-        var targetRotation = Quaternion.LookRotation(_player.transform.position - transform.position);
+
+        if (!_firstTime) {
+            FollowPlayer(_player.transform.position);
+        }
+        else {
+            FollowPlayer(_oldPlayerPosition);
+            _firstTime = false;
+        }
+    }
+
+    private void WaitAndTurnOnFlashLight() {
+        if (elapsed < _waitBeforeTurnOnFlashlight) {
+            return;
+        }
+        else {
+            if (!_lightSpotter.enabled) {
+                _lightSpotter.enabled = true;
+                _flashlight.enabled = true;
+            }
+        }
+    }
+
+    private void FollowPlayer(Vector3 playerPosition) {
+        var targetRotation = Quaternion.LookRotation(playerPosition - transform.position);
         var str = Mathf.Min(strength * Time.deltaTime, 1);
         _flashlight.transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, str);
-
-        elapsed += Time.deltaTime;
     }
 
     private void OnEnable() {
-        _lightSpotter.enabled = true;
-        _flashlight.enabled = true;
+        _oldPlayerPosition = _player.transform.position;
+        _flashlight.transform.rotation = Quaternion.LookRotation(_oldPlayerPosition);
         elapsed = 0.0f;
     }
 
