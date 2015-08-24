188ec1376e58b2a351cc9cd614c65eebfc2dd0f7
almyu <hire@indeed.li>
2015-08-24 13:13:28 +0600

    Add trigger bar

---
+++ Assets/LD33/Scripts/GUILogics/TriggerBar.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(RectTransform))]
+public class TriggerBar : MonoSingleton<TriggerBar> {
+
+    public RectTransform template;
+
+    private void Awake() {
+        var triggers = FindObjectsOfType<Trigger>();
+
+        for (int i = 0; i < triggers.Length; ++i)
+            InstantiateToggle(triggers[i], i);
+    }
+
+    private void InstantiateToggle(Trigger trigger, int position) {
+        var xf = Instantiate(template);
+        xf.pivot = xf.pivot.WithX(position);
+
+        var toggle = xf.GetComponentInChildren<Toggle>();
+        if (toggle) trigger.onTrigger.AddListener(() => toggle.isOn = true);
+    }
+}
