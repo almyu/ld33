5506bce510cf0c751bcaaee2e8c25824de565e88
almyu <hire@indeed.li>
2015-08-25 01:40:53 +0600

    Add player step sfx script

---
+++ Assets/LD33/Scripts/SFX/PlayerStepSfx.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using JamSuite.Audio;
+
+public class PlayerStepSfx : MonoBehaviour {
+
+    public string sfxName = "Step";
+    public Vector2 volumeRange = new Vector2(0.8f, 1f);
+
+    public void PlayStep() {
+        Sfx.Play(sfxName, Random.Range(volumeRange[0], volumeRange[1]));
+    }
+}
