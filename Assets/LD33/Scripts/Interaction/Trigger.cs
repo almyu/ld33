68c21d8b58f6cbb65125cfc24998c8ac15965d7f
almyu <hire@indeed.li>
2015-08-25 04:38:36 +0600

    Assign boo sounds to triggers

--- Assets/LD33/Scripts/Interaction/Trigger.cs
+++ Assets/LD33/Scripts/Interaction/Trigger.cs
@@ -1,17 +1,23 @@
 ﻿using UnityEngine;
 using UnityEngine.Events;
 using JamSuite.UI;
+using JamSuite.Audio;
 
 [RequireComponent(typeof(SphereCollider))]
 public class Trigger : MonoBehaviour {
 
     public string text = "Boo!";
+    public string[] sfxVariants;
     public UnityEvent onTrigger;
 
     private void OnTriggerEnter(Collider other) {
         FloatingText.instance.Spawn(transform.position, 0).text = text;
         onTrigger.Invoke();
         ScoreCounter.instance.BooPointFound();
+
+        if (sfxVariants.Length != 0)
+            Sfx.Play(sfxVariants[Random.Range(0, sfxVariants.Length)]);
+
         Destroy(gameObject);
     }
 
