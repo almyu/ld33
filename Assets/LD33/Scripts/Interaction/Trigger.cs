c9d636b34eac63ec88be38815fc049ea71bbc7d9
almyu <hire@indeed.li>
2015-08-24 12:24:16 +0600

    Add boo trigger (also known as ook trigger)

---
+++ Assets/LD33/Scripts/Interaction/Trigger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using JamSuite.UI;
+
+public class Trigger : MonoBehaviour {
+
+    public string text = "Boo!";
+    public UnityEvent onTrigger;
+
+    private void OnTriggerEnter(Collider other) {
+        FloatingText.instance.Spawn(transform.position, 0).text = text;
+        onTrigger.Invoke();
+        Destroy(gameObject);
+    }
+}
