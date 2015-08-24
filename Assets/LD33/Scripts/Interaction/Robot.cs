746c6abdbbe8921e36f63259c0c0fd656f3a0896
almyu <hire@indeed.li>
2015-08-25 03:57:31 +0600

    Assign sfxes

--- Assets/LD33/Scripts/Interaction/Robot.cs
+++ Assets/LD33/Scripts/Interaction/Robot.cs
@@ -5,10 +5,12 @@ public class Robot : MonoBehaviour {
 
     public Animation headAnimation;
     public string alarmSfx = "RobotSpotsPlayer";
+    public float alarmSfxThrottle = 3f;
+    public Vector2 alarmSfxVolumeRange = new Vector2(0.5f, 0.7f);
 
     public void OnPlayerSpotted() {
         SetAnimationSpeed(headAnimation, 0f);
-        Sfx.Play(alarmSfx);
+        Sfx.Play(alarmSfx, Random.Range(alarmSfxVolumeRange[0], alarmSfxVolumeRange[1]), alarmSfxThrottle);
     }
 
     public void OnPlayerLost() {
