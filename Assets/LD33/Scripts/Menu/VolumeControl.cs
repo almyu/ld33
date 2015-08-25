d08e5e60309aa7c98fc84224168e53f93570f143
almyu <hire@indeed.li>
2015-08-25 06:17:35 +0600

    Add volume control script

---
+++ Assets/LD33/Scripts/Menu/VolumeControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class VolumeControl : MonoBehaviour {
+
+    public AudioMixer mixer;
+
+    private void Start() {
+        if (PlayerPrefs.HasKey("MusicVolume")) mixer.SetFloat("MusicVolume", PlayerPrefs.GetFloat("MusicVolume"));
+        if (PlayerPrefs.HasKey("EffectsVolume")) mixer.SetFloat("EffectsVolume", PlayerPrefs.GetFloat("EffectsVolume"));
+    }
+
+    public void SetMusicVolume(float vol) {
+        mixer.SetFloat("MusicVolume", vol);
+        PlayerPrefs.SetFloat("MusicVolume", vol);
+    }
+
+    public void SetEffectsVolume(float vol) {
+        mixer.SetFloat("EffectsVolume", vol);
+        PlayerPrefs.SetFloat("EffectsVolume", vol);
+    }
+}
