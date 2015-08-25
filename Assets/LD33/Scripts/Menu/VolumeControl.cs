55a5ac933f7ad58be99d8b07564e3b35075ea7fc
almyu <hire@indeed.li>
2015-08-25 06:21:16 +0600

    Add sliders to volume control script

--- Assets/LD33/Scripts/Menu/VolumeControl.cs
+++ Assets/LD33/Scripts/Menu/VolumeControl.cs
@@ -1,13 +1,27 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Audio;
 
 public class VolumeControl : MonoBehaviour {
 
     public AudioMixer mixer;
+    public Slider musicVolumeSlider, effectsVolumeSlider;
 
     private void Start() {
         if (PlayerPrefs.HasKey("MusicVolume")) mixer.SetFloat("MusicVolume", PlayerPrefs.GetFloat("MusicVolume"));
         if (PlayerPrefs.HasKey("EffectsVolume")) mixer.SetFloat("EffectsVolume", PlayerPrefs.GetFloat("EffectsVolume"));
+
+        musicVolumeSlider.onValueChanged.AddListener(SetMusicVolume);
+        effectsVolumeSlider.onValueChanged.AddListener(SetEffectsVolume);
+
+        var musicVolume = 0f;
+        var effectsVolume = 0f;
+
+        mixer.GetFloat("MusicVolume", out musicVolume);
+        mixer.GetFloat("EffectsVolume", out effectsVolume);
+
+        musicVolumeSlider.value = musicVolume;
+        effectsVolumeSlider.value = effectsVolume;
     }
 
     public void SetMusicVolume(float vol) {
